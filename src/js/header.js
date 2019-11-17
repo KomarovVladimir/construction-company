c4c849eff772f08efc01f3a6d258a7dbde112a90
Vladimir <komarov2142@gmail.com>
2019-11-17 16:56:00 +1000

    new file structure

--- src/js/header.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$(window).scroll(function(){
-    if($(window).scrollTop() >= $('.header').outerHeight()) {
-        $('.header').addClass('header--scrolled')
-    } else {
-        $('.header').removeClass('header--scrolled')
-    }
-});
