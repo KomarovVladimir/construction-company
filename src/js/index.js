2726ec3835c5826fc5e5976f332717f35f450622
Vladimir <komarov2142@gmail.com>
2019-11-17 20:28:46 +1000

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,7 @@
 // STYLES
 import 'normalize.css';
 import '@cmyee/pushy/scss/pushy.scss';
-import './scss/style.scss';
+import '../scss/main.scss';
 
 //JS
 import '@cmyee/pushy/js/pushy';
-import './js/portfolio';
-import './js/questions';
-import './js/header';
