d88dfce0eb75b125f2e0b3d04bc3575d217a88ca
Vladimir <komarov2142@gmail.com>
2019-11-25 15:46:29 +1000

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ const config = {
         use: 'url-loader'
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
