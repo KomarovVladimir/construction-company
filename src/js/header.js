e4aff1b3aad8ff8c6519da7a433a7b90dad9f8aa
Vladimir <komarov2142@gmail.com>
2019-11-27 14:48:32 +1000

    header js

--- src/js/header.js
+++ src/js/header.js
@@ -3,6 +3,10 @@ import $ from 'jquery';
 $(function() {
     'use strict'
 
+    if($(window).scrollTop() >= $('.header').outerHeight()) {
+        $('.header').addClass('header_scrolled')
+    }
+
     $(window).scroll(function(){
         if($(window).scrollTop() >= $('.header').outerHeight()) {
             $('.header').addClass('header_scrolled')
@@ -13,5 +17,9 @@ $(function() {
 
     $('#header-btn').click(function() {
         $('.header').toggleClass('header_mobile_show')
+    });
+
+    $('.header__nav-item').click(() => {
+        $('.header').removeClass('header_mobile_show');
     });
 })
