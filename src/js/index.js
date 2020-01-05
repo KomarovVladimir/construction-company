bf01c4430e0d3271c56b82505cff1b91e2105eca
Vladimir <komarov2142@gmail.com>
2020-01-05 18:21:03 +1000

    fa local

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import 'normalize.css';
 import '@cmyee/pushy/scss/pushy.scss';
 import '../scss/main.scss';
 import 'animate.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 //JS
 import 'owl.carousel';
