8c6b3e7a01e7e2fd876101eb7f8ee09ef99bcf8a
Vladimir <komarov2142@gmail.com>
2020-01-07 13:17:31 +1000

    minified vendors

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,15 @@
 // STYLES
 import 'normalize.css';
-import '@cmyee/pushy/scss/pushy.scss';
 import '../scss/main.scss';
-import 'animate.css'
+import 'animate.css/animate.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 //JS
-import 'owl.carousel';
+import 'owl.carousel/dist/owl.carousel.min.js';
 import './header';
 import './portfolio';
 import './reviews,';
 import './faq';
-import WOW from 'wow.js';
+import WOW from 'wow.js/dist/wow.min.js';
 
 new WOW().init();
